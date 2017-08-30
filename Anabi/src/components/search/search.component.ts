95013b16aaae25bf9fb052a0a95317568c94f698
Radu Ungureanu <radu.ungureanu@outlook.com>
2017-08-30 10:23:04 +0300

    ANABI-35-search-decision - call search endpoint and display results in UI

--- Anabi/src/components/search/search.component.ts
+++ Anabi/src/components/search/search.component.ts
@@ -1,16 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+
+import { DecisionsHttp } from 'shared/http/decisions.http'
+
+import { DecisionSummary } from 'shared/models/decisionSummary.model'
 
 @Component({
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  providers: [DecisionsHttp]
 })
 export class SearchComponent implements OnInit {
   public page;
+  results: DecisionSummary[];
+  decisionsHttp: DecisionsHttp;
 
-  constructor() {
+  constructor(decisionsHttp: DecisionsHttp) {
+    this.decisionsHttp = decisionsHttp;
   }
 
   ngOnInit() {
     this.page = 'search page';
+    this.decisionsHttp.search().subscribe(val => this.results = val);
   }
 }
