4be8996c58b8c2670d4927015360515d00d76d01
Alexandru Albu <afalex89@gmail.com>
2018-02-10 01:36:25 +0200

    ANABI-112: update #1 design

--- src/material.module.ts
+++ src/material.module.ts
@@ -1,15 +1,19 @@
 import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatTabsModule} from '@angular/material/tabs';
 
 const modules = [
+  BrowserAnimationsModule,
   MatToolbarModule,
   MatMenuModule,
+  MatIconModule,
   MatListModule,
   MatButtonModule,
   MatInputModule,
