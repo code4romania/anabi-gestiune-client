4c2ddffd40fd3249f352e51df978319ad331d378
oproiustefan <oproiustefan@yahoo.com>
2019-02-23 14:57:07 +0200

    adds defendant model

---
+++ src/app/core/models/defendant.model.ts
@@ -0,0 +1,11 @@
+export class Defendant {
+    idNumber: string;
+    idSerie: string;
+    identification: string;
+    isPerson: boolean;
+    name: string;
+    birthdate: string;
+    firstName: string;
+    identifierId: number;
+    nationality: string;
+}
