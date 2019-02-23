19ba9218fbc4a00c780546fb52c5c1d2c01100db
oproiustefan <oproiustefan@yahoo.com>
2019-02-23 17:10:54 +0200

    saved progress 17:10

--- src/app/assets/components/add-defendant/add-defendant.component.ts
+++ src/app/assets/components/add-defendant/add-defendant.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Defendant } from '@app/core/models/defendant.model';
 
 @Component({
   selector: 'app-add-defendant',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddDefendantComponent implements OnInit {
 
+  @Output() defendantAdd: EventEmitter<Defendant> = new EventEmitter<Defendant>();
+
+  defendantTypeOptions: string[] = [
+    'Persoana Fizica',
+    'Persoana Juridica',
+  ];
+
+  selectedDefendantTypeOption: string;
+  defendantForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.defendantForm = new FormGroup({
+      defendantType: new FormControl(''),
+      pfLastName: new FormControl(''),
+      pfFirstName: new FormControl(''),
+      pfNationality: new FormControl(''),
+      pfIdentifier: new FormControl(''),
+      pfBirthDate: new FormControl(''),
+    });
+  }
+
+  get defendantType(): string {
+    return this.defendantForm.value.defendantType;
+  }
+
+  onSubmit() {
+
   }
 
 }
