11569450ede715fa6e1507e6936a4c0be9ed59d6
Wydder <bogdan.mosica@gmail.com>
2019-05-11 23:17:32 +0300

    Quick fix on address view html file

--- src/app/assets/components/view-address/view-address.component.ts
+++ src/app/assets/components/view-address/view-address.component.ts
@@ -4,7 +4,7 @@ import { Address } from '@app/core';
 @Component({
   selector: 'app-view-address',
   templateUrl: './view-address.component.html',
-  styleUrls: ['./view-address.component.scss']
+  styleUrls: ['./view-address.component.scss'],
 })
 export class ViewAddressComponent {
   @Input() address: Address;
